82a55037bbc6e63f9a597bc1d637b0ab72111fc2
SangramTester <sangram316@yahoo.com>
2025-12-30 22:23:20 -0800

    update dataset loading

--- src/ml/dataset.py
+++ src/ml/dataset.py
@@ -1,4 +1,6 @@
+from pytorch_lightning.utilities.types import EVAL_DATALOADERS
 import torch
+import torchvision
 from torch.utils.data import Dataset, DataLoader
 from torchvision import datasets, transforms
 import pytorch_lightning as pl
@@ -9,7 +11,26 @@ class CIFAR10DataModule(pl.LightningDataModule):
         self.data_dir = data_dir
         self.batch_size = batch_size
 
+
+    def prepare_data(self):
+        torchvision.datasets.CIFAR10(self.data_dir, train=True, download=True)
+        torchvision.datasets.CIFAR10(self.data_dir, train=False, download=True)
+
+    def setup(self, stage=None):
+        transform = transforms.Compose(
+            [transforms.ToTensor(),
+             transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))]
+        )
+        self.trainset = torchvision.datasets.CIFAR10(self.data_dir, train=True, transform=transform)
+        self.testset = torchvision.datasets.CIFAR10(self.data_dir, train=False, transform=transform)
+
+    def train_dataloader(self):
+        return DataLoader(self.trainset, batch_size=self.batch_size, shuffle=True)
     
-        self.trainset = torchvision.datasets.CIFAR10(root='./data', train=True, download=True, transform=transform)
-        self.trainloader = torch.utils.data.DataLoader(trainset, batch_size=batch_size, shuffle=True, num_workers=2)
-        self.testset = torchvision.datasets.CIFAR10(root='./data', train=False, download=True, transform=transform)
+    def test_dataloader(self):
+        return DataLoader(self.testset, batch_size=self.batch_size, shuffle=False)
+
+    def classes(self):
+      return ('plane', 'car', 'bird', 'cat',
+           'deer', 'dog', 'frog', 'horse', 'ship', 'truck')
+    
