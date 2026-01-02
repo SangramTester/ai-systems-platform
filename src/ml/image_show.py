956592925247b56a880dbc40f3ccf15f7f554674
SangramTester <sangram316@yahoo.com>
2026-01-01 16:03:42 -0800

    separate model and training code. add test model.

--- src/ml/image_show.py
+++ src/ml/image_show.py
@@ -10,20 +10,20 @@ def imshow(img):
     plt.show()
 
 
-batch_size = 32
-# get some random training images
-dm = CIFAR10DataModule(data_dir="./data", batch_size=32)
-dm.prepare_data()          # downloads
-dm.setup(stage=None)       # builds datasets
+# batch_size = 32
+# # get some random training images
+# dm = CIFAR10DataModule(data_dir="./data", batch_size=32)
+# dm.prepare_data()          # downloads
+# dm.setup(stage=None)       # builds datasets
 
-train_loader = dm.train_dataloader()
-dataiter = iter(train_loader)
-images, labels = next(dataiter)
+# train_loader = dm.train_dataloader()
+# dataiter = iter(train_loader)
+# images, labels = next(dataiter)
 
 
-# print labels
-classes = dm.trainset.classes
-print(' '.join(f'{classes[labels[j]]:5s}' for j in range(batch_size)))
+# # print labels
+# classes = dm.trainset.classes
+# print(' '.join(f'{classes[labels[j]]:5s}' for j in range(batch_size)))
 
-# show images
-imshow(torchvision.utils.make_grid(images))
+# # show images
+# imshow(torchvision.utils.make_grid(images))
